8be84aa7069bafbd0c21d234ee9def265366b157
Will Beasley <wibeasley@hotmail.com>
2023-02-04 14:04:10 -0600

    provide outcome message
    and option to show more entries
    
    ref #493

--- scripts/url-db-from-ctv-md.R
+++ scripts/url-db-from-ctv-md.R
@@ -1,8 +1,10 @@
-url_db_from_ctv_md <- function(path_md, verbose = TRUE) {
+url_db_from_ctv_md <- function(path_md, verbose = TRUE, verbose_row_count = 10L) {
   if (!inherits(path_md, "character"))  stop("`path_md` must be a character.")
   if (length(path_md) != 1L)            stop("Only one `path_md` value is accepted.")
   if (!inherits(verbose, "logical"))    stop("`verbose` must be a character.")
   if (length(verbose) != 1L)            stop("Only one `verbose` value is accepted.")
+  if (!is.numeric(verbose_row_count))   stop("`verbose_row_count` must be numeric")
+  if (length(verbose_row_count) != 1L)  stop("Only one `verbose_row_count` value is accepted.")
 
   # Capture each url, even those with parentheses.  See https://stackoverflow.com/a/67942420/1082435
   pattern <- "\\[(?<page_name>.[^][]+)\\](\\((?<page_url>(?:[^()]+|(?2))+)\\))"
@@ -30,13 +32,21 @@ url_db_from_ctv_md <- function(path_md, verbose = TRUE) {
 
   if (verbose) {
     cat("---- URLs to check ----------\n")
-    print(db)
+    print(db, n = verbose_row_count)
   }
 
   db$Parent <- path
   class(db) <- "url_db"
 
-  tools:::check_url_db(db, verbose = verbose, parallel = TRUE)
+  output <- tools:::check_url_db(db, verbose = verbose, parallel = TRUE)
+  
+  if (nrow(output) == 0L) {
+    message("All links in the task view resolved successfully.")
+  } else {
+    warning("At least one link in the task view needs attention.")
+  }
+  
+  output
 }
 
-# url_db_from_ctv_md("WebTechnologies.md")
+# url_db_from_ctv_md("WebTechnologies.md", verbose_row_count = 20)
