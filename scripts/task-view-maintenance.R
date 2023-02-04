726fd0cd28e5fc3b841fd91d12acc417b575ba73
Will Beasley <wibeasley@hotmail.com>
2023-02-04 12:28:35 -0600

    start maintenance script
    ref #495

---
+++ scripts/task-view-maintenance.R
@@ -0,0 +1,27 @@
+# Keep current with the advice in 
+#  https://github.com/cran-task-views/ctv/blob/main/Contributing.md
+
+task_view_name  <- "WebTechnologies"
+path_md         <- sprintf("%s.md"  , task_view_name)
+path_html       <- sprintf("%s.html", task_view_name)
+
+if (base::basename(getwd()) != "WebTechnologies") {
+  stop("The working directory should be the root of the repository.")
+} else {
+  source("scripts/url-db-from-ctv-md.R")
+}
+
+# Create the html and visually inspect the content and formatting.
+ctv::ctv2html(path_md)
+utils::browseURL(path_html)
+
+# Check that the information text and the package list 
+#  are consistent and that all packages are available from CRAN:
+ctv::check_ctv_packages(path_md)
+
+# Determine if any urls need to be updated
+#  or if any services are defunct (and their entry should be removed).
+url_db_from_ctv_md(path_md)
+
+# Check spelling
+spelling::spell_check_files(path_md)
