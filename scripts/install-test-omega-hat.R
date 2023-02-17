c85ea8fcdf24c3b9e4c5866fb0e76ceb3ac359b2
Will Beasley <wibeasley@hotmail.com>
2023-02-17 17:34:38 -0600

    first results from omegahat repo
    ref #500

--- scripts/install-test-omega-hat.R
+++ scripts/install-test-omega-hat.R
@@ -5,7 +5,8 @@
 # .libPaths(c(libraries_existing, library_temp))
 
 # cran_dependencies <- c(
-#   "RCurl"
+#   "RCurl",
+#   "RJSONIO"
 # )
 # 
 # install.packages(cran_dependencies)#, lib = library_temp)
@@ -40,7 +41,8 @@ for(p in package) {
     p, 
     repos = "http://www.omegahat.net/R",
     type  = "source"
-    )#, lib = library_temp)
+    #, lib = library_temp
+  )
   
   success <- requireNamespace(p, quietly = T)
   outcome[[p]] <- success
@@ -48,9 +50,18 @@ for(p in package) {
   message("success: ", success)
   if (success) remove.packages(p)#, lib = library_temp)
 }
+
+
 outcome |> 
-  tibble::as_tibble()
-print(outcome)
+  tibble::enframe(
+    name = "package",
+    value = "install_success"
+  ) |> 
+  dplyr::mutate(
+    install_success = as.logical(install_success)
+  )
+
+# print(outcome)
 
 # Remove all the CRAN packages used to test Omega Hat
 # remove.packages(cran_dependencies)#, lib = library_temp)
@@ -58,3 +69,30 @@ print(outcome)
 # Restore session's libraries and remove temp library
 # .libPaths(libraries_existing)
 # unlink(library_temp, recursive = TRUE, force = TRUE)
+
+# install.packages("SpiderMonkey", repos = "http://www.omegahat.net/R", type  = "source")
+# remotes::install_github("omegahat/SpiderMonkey")
+
+# # A tibble: 20 × 2
+#    package        install_success
+#    <chr>          <lgl>          
+#  1 CGIwithR       TRUE           
+#  2 R2GoogleMaps   TRUE           
+#  3 RAmazonDBREST  TRUE           
+#  4 Rcompression   FALSE      # Please define LIB_ZLIB    
+#  5 RDCOMClient    TRUE           
+#  6 RDCOMServer    FALSE      # package ‘RDCOMServer’ is not available for this version of R    
+#  7 Rflickr        TRUE           
+#  8 RGoogleDocs    TRUE           
+#  9 RGoogleStorage TRUE           
+# 10 RGoogleTrends  TRUE           
+# 11 RHTMLForms     TRUE           
+# 12 RTidyHTML      FALSE      # make: cc: No such file or directory; make: *** [<builtin>: access.o] Error 127    
+# 13 RUbigraph      FALSE      # undefined exports: runUbigraph    
+# 14 SpiderMonkey   FALSE      # ERROR: dependencies 'RAutoGenRunTime', 'Rffi' are not available for package 'SpiderMonkey    
+# 15 SSOAP          TRUE           
+# 16 SXalan         FALSE          
+# 17 Sxslt          FALSE          
+# 18 WADL           TRUE           
+# 19 XMLRPC         TRUE           
+# 20 XMLSchema      TRUE 
