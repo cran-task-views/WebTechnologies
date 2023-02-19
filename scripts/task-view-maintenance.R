6c77f677d86ade9084170ee5db36d7db68b93b3d
Will Beasley <wibeasley@hotmail.com>
2023-02-19 14:55:48 -0600

    a little more ergonomic spelling maintenance
    ref #499

--- scripts/task-view-maintenance.R
+++ scripts/task-view-maintenance.R
@@ -29,7 +29,9 @@ words_ignore <- readr::read_lines(path_spelling)
 spelling::spell_check_files(path_md, words_ignore)
 
 # Uncomment to update list.
-# path_md |>
-#   spelling::spell_check_files() |>
-#   dplyr::pull("word") |>
-#   readr::write_lines(path_spelling)
+if (FALSE) {
+  path_md |>
+    spelling::spell_check_files() |>
+    dplyr::pull("word") |>
+    readr::write_lines(path_spelling)
+}
